4d59a9a344762b2fe95c3f57ecfe10eb5bdbb8fd
Mikhail Zykov <paradoxm@ya.ru>
2023-08-02 22:11:23 +0500

    added task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine($"Числа равны");
+}
+else
+{
+    double min = number1 < number2 ? number1 : number2;
+    double max = number1 > number2 ? number1 : number2;
+    Console.WriteLine($"Число {max} больше числа {min}");
+}
