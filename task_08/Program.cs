4ec297af53fb1db69a627c0fedab82617bcc1358
Mikhail Zykov <paradoxm@ya.ru>
2023-08-02 23:05:14 +0500

    added task_08

---
+++ task_08/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("Ошибка! Введено некорректное число");
+}
+else if (number < 2)
+{
+    Console.WriteLine("Чётные числа отсутствуют");
+}
+else if (number >= 0)
+{
+    Console.WriteLine($"Чётные числа от 1 до {number}");
+
+    for (int i = 1; i <= number; i++)
+    {
+        bool isEven = i % 2 == 0;
+
+        if (isEven)
+        {
+            Console.Write($"{i} ");
+        }
+
+    }
+    Console.WriteLine("");
+}
+
+
+
