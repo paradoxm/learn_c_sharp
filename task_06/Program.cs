cee2aed00d4802e53ab76c0b103e81ab77e5b3b1
Mikhail Zykov <paradoxm@ya.ru>
2023-08-02 22:53:31 +0500

    added task_06

---
+++ task_06/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число, чтобы проверить является ли оно четным: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool isEven = number % 2 == 0;
+
+if (isEven)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
