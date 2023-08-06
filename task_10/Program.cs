da9ffa27160d8ab5b3b6058ba06579a7a178d535
Mikhail Zykov <paradoxm@ya.ru>
2023-08-06 20:51:50 +0500

    added task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int positiveNumber = Math.Abs(number);
+
+bool isValid = ValidateNumber(positiveNumber);
+
+if (isValid)
+{
+    int secondDigit = GetSecondDigit(positiveNumber);
+
+    Console.WriteLine($"Вторая цифра числа: {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
+
+bool ValidateNumber(int num)
+{
+    return num >= 100 && num <= 999;
+}
+
+int GetSecondDigit(int num)
+{
+    return num / 10 % 10;
+}
