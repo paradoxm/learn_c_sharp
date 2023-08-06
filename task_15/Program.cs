b3b2dc72a2356cb204996882988fa5034559dbb3
Mikhail Zykov <paradoxm@ya.ru>
2023-08-06 21:09:52 +0500

    added task_15

---
+++ task_15/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool isValid = ValidateNumber(number);
+
+if (isValid)
+{
+    bool isWeekend = NumberIsWeekend(number);
+    string result = isWeekend ? "да" : "нет";
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректный номер дня недели");
+}
+
+
+bool ValidateNumber(int num)
+{
+    return num >= 1 && num <= 7;
+}
+
+bool NumberIsWeekend(int num)
+{
+    return num == 6 || num == 7;
+}
