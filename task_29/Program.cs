aadefd38b4822e8a0fca2f1c38edeef63aca5b78
Mikhail Zykov <paradoxm@ya.ru>
2023-08-28 20:57:19 +0500

    added task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+Console.WriteLine("Необходимо ввести массив из 8 чисел");
+int[] userList = ReadNumbers(8);
+
+PrintArray(userList);
+
+
+
+int[] ReadNumbers(uint count)
+{
+    int[] list = new int[count];
+
+    for (int index = 0; index < count; index++)
+    {
+        Console.Write($"Введите {index + 1}: ");
+
+        var number = Convert.ToInt32(Console.ReadLine());
+        list[index] = number;
+    }
+
+    return list;
+}
+
+
+void PrintArray(int[] list)
+{
+    string joinedList = string.Join(",", list);
+
+    Console.WriteLine($"[{joinedList}]");
+}
