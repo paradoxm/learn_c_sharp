247906189f1b565a940a075b9dbe1ab21c7882cf
Mikhail Zykov <paradoxm@ya.ru>
2023-09-16 10:06:22 +0500

    refactor task_68

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -9,7 +9,7 @@ using LearnCSharpUtils;
 int numN = Number.Propmpt("Введите начало диапазона: ");
 int numM = Number.Propmpt("Введите конец диапазона: ");
 
-if (!isValid(numN, numM)) return;
+if (!IsValid(numN, numM)) return;
 
 int result1 = Ackerman1(numN, numM);
 int result2 = Ackerman2(numN, numM);
@@ -20,7 +20,7 @@ Console.WriteLine($"Результат функции Аккермана2: A({nu
 
 
 
-bool isValid(int from, int to)
+bool IsValid(int from, int to)
 {
     if (from < 1 || to < 0)
     {
@@ -35,7 +35,7 @@ bool isValid(int from, int to)
 int Ackerman1(int n, int m)
 {
     if (n == 0) return m + 1;
-    else if (m == 0) return Ackerman1(n - 1, 1);
+    if (m == 0) return Ackerman1(n - 1, 1);
 
     return Ackerman1(n - 1, Ackerman1(n, m - 1));
 }
